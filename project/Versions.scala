1ba5b0d2f719cb6c1250678ade818fe26ea03ed7
Nicolas F. Rouquette <nicolas.f.rouquette@jpl.nasa.gov>
2015-06-15 02:20:49 +0000

    OTI/Core version 853 OTI/Trees version 854 OTI/ChangeMigration version 855 Artifact packaging for JPL
    git-svn-id: https://www.omgwiki.org/repos/TIWG/Eclipse%20Scala%20Projects/org.omg.oti.magicdraw@856 b05f3425-8805-43f2-8a1d-5852a8693556

--- project/Versions.scala
+++ project/Versions.scala
@@ -22,19 +22,19 @@ object Versions {
   // OTI Core version
     
   val oti_core_prefix = "0.14.0"
-  val oti_core_suffix = "849"
+  val oti_core_suffix = "853"
   val oti_core_version = oti_core_prefix+"-"+oti_core_suffix
 
   // OTI Change Migration version
     
   val oti_changeMigration_prefix = "0.14.0"
-  val oti_changeMigration_suffix = "851"
+  val oti_changeMigration_suffix = "855"
   val oti_changeMigration_version = oti_changeMigration_prefix+"-"+oti_changeMigration_suffix
 
   // OTI Trees version
     
   val oti_trees_prefix = "0.14.0"
-  val oti_trees_suffix = "850"
+  val oti_trees_suffix = "854"
   val oti_trees_version = oti_trees_prefix+"-"+oti_trees_suffix
 
 }
