896c6e6aaa2173a7d0f1445e746ad68c8e7bf14f
Nicolas F. Rouquette <nicolas.f.rouquette@jpl.nasa.gov>
2015-06-17 15:04:40 +0000

    OTI/Core: 909 OTI/Trees: 910 OTI/ChangeMigration: 911
    git-svn-id: https://www.omgwiki.org/repos/TIWG/Eclipse%20Scala%20Projects/org.omg.oti.magicdraw@912 b05f3425-8805-43f2-8a1d-5852a8693556

--- project/Versions.scala
+++ project/Versions.scala
@@ -22,19 +22,19 @@ object Versions {
   // OTI Core version
     
   val oti_core_prefix = "0.14.0"
-  val oti_core_suffix = "896"
+  val oti_core_suffix = "909"
   val oti_core_version = oti_core_prefix+"-"+oti_core_suffix
 
   // OTI Change Migration version
     
   val oti_changeMigration_prefix = "0.14.0"
-  val oti_changeMigration_suffix = "898"
+  val oti_changeMigration_suffix = "911"
   val oti_changeMigration_version = oti_changeMigration_prefix+"-"+oti_changeMigration_suffix
 
   // OTI Trees version
     
   val oti_trees_prefix = "0.14.0"
-  val oti_trees_suffix = "897"
+  val oti_trees_suffix = "910"
   val oti_trees_version = oti_trees_prefix+"-"+oti_trees_suffix
 
 }
