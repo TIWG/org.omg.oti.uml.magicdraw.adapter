d9cc01c2ea024b33b59ba5c832cc386a96ace2d9
Nicolas F Rouquette <nicolas.f.rouquette@jpl.nasa.gov>
2016-02-01 19:30:14 +0000

    dependencies

--- project/Versions.scala
+++ project/Versions.scala
@@ -2,7 +2,7 @@
 object Versions {
 
   // this project
-  val version = "445390.5"
+  val version = "445390.6"
 
   // https://github.jpl.nasa.gov/imce/org.omg.oti.changeMigration
   val oti_uml_change_migration="445380.3"
@@ -14,5 +14,5 @@ object Versions {
   val oti_uml_canonical_xmi_loader="445386.2"
 
   // https://github.jpl.nasa.gov/imce/jpl-dynamicscripts-magicdraw-plugin
-  val dynamic_scripts_plugin="2.2"
+  val dynamic_scripts_plugin="2.3"
 }
