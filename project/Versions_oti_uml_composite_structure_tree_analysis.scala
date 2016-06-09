4059b1580bb803ff7495710d66be06bd21d88f7d
Nicolas F Rouquette <nicolas.f.rouquette@jpl.nasa.gov>
2016-06-09 13:21:48 +0000

    IMCEI-186 - Build a beta release of the OMF exporter - turn off the canonicalName mapping - dependencies

--- project/Versions_oti_uml_composite_structure_tree_analysis.scala
+++ project/Versions_oti_uml_composite_structure_tree_analysis.scala
@@ -1,5 +1,5 @@
 object Versions_oti_uml_composite_structure_tree_analysis {
 
   // https://github.jpl.nasa.gov/imce/org.omg.oti.uml.composite_structure_tree_analysis
-  val version="2.2.0"
+  val version="2.3.0"
 }
