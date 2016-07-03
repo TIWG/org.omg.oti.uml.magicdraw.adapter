ef8778d9d6750a58272f9a9b7283e640945c83e5
Nicolas F Rouquette <nicolas.f.rouquette@jpl.nasa.gov>
2016-07-03 23:29:23 +0000

    IMCEI-255 - DynamicScripts plugin should look for libraries if .classpath output directory doesn't exist dependencies

--- project/Versions.scala
+++ project/Versions.scala
@@ -2,6 +2,6 @@
 object Versions {
 
   // this project
-  val version = "2.7.2"
+  val version = "2.7.3"
 
 }
--- project/Versions_imce_md18_0_sp5_dynamic_scripts.scala
+++ project/Versions_imce_md18_0_sp5_dynamic_scripts.scala
@@ -1,5 +1,5 @@
 object Versions_imce_md18_0_sp5_dynamic_scripts {
 
   // https://github.jpl.nasa.gov/imce/imce.dynamic_scripts.magicdraw.plugin
-  val version="5.11.1"
+  val version="5.11.2"
 }
